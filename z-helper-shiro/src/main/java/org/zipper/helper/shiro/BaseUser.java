4e4a7c0f55f27d67999a0890f456259e6fcc75fa
zhuxj <870324066@qq.com>
2020-07-03 11:22:30 +0800

    add shiro

---
+++ z-helper-shiro/src/main/java/org/zipper/helper/shiro/BaseUser.java
@@ -0,0 +1,22 @@
+package org.zipper.helper.shiro;
+
+public abstract class BaseUser {
+    protected String username;
+    protected String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
